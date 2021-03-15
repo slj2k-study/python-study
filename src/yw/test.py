cef9bfdd4df4f283e8b84d943fc8b02f5a6ea5ab
IdiotOldMan <teleponkr@daum.net>
2021-03-15 23:20:59 +0900

    pygame

--- src/yw/test.py
+++ src/yw/test.py
@@ -1,2 +1,18 @@
-print('Hello World')
-print('Testing Commit')
+import pygame
+
+pygame.init()
+
+v_screen_width = 480 #가로 크기
+v_screen_height = 640 #세로 크기
+
+screen = pygame.display.set_mode((v_screen_width, v_screen_height))
+
+pygame.display.set_caption(("My Game"))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
