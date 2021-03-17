7d3ab9c55cbf3de8d10aa255feb6c17e87d96b76
IdiotOldMan <teleponkr@daum.net>
2021-03-17 23:09:07 +0900

    20210317

--- src/yw/test.py
+++ src/yw/test.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-v_screen_width = 480 #가로 크기
-v_screen_height = 640 #세로 크기
+v_screen_width = 480
+v_screen_height = 640
 
 screen = pygame.display.set_mode((v_screen_width, v_screen_height))
 
@@ -12,7 +12,7 @@ pygame.display.set_caption(("My Game"))
 running = True
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pygame.event.get(): # 이벤트 발생 체크
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트 발생
             running = False
 pygame.quit()
